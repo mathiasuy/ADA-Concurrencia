74b2eb131732d13c9b9e542f1407457c949956cb
Mathias Battistella <mathias.battistella.contractor@bbva.com>
2019-05-12 19:53:00 -0300

    resumido en un task

--- main.adb
+++ main.adb
@@ -4,54 +4,33 @@ with Ada.Task_Identification;  use Ada.Task_Identification;
 
 procedure main is
 
-   task type Tarea1(p : natural);
-   task body Tarea1 is
+    task type Tarea(p: natural);
+    task body Tarea is
     begin
       for I in 1..5 loop
-            put_line("Tarea 1 Línea 1");
             put(p);
-            put_line("Tarea 1 Línea 2");
+            put_line(": Línea 1");
             put(p);
-            put_line("Tarea 1 Línea 3");
+            put_line(": Línea 2");
             put(p);
-            put_line("Tarea 1 Línea 4");
+            put_line(": Línea 3");
             put(p);
-            put_line("Tarea 1 Línea 5");
+            put_line(": Línea 4");
             put(p);
-            new_line;
-            delay 10.5;
-        end loop;
-    end Tarea1;
-
-    type t_tarea1 is access Tarea1;
-
-    task type Tarea2(p: natural);
-    task body Tarea2 is
-    begin
-      for I in 1..5 loop
-            put_line("Tarea 2 Línea 1");
-            put(p);
-            put_line("Tarea 2 Línea 2");
-            put(p);
-            put_line("Tarea 2 Línea 3");
-            put(p);
-            put_line("Tarea 2 Línea 4");
-            put(p);
-            put_line("Tarea 2 Línea 5");
-            put(p);
+            put_line(": Línea 5");
          new_line;
          delay 10.5;
         end loop;
-    end Tarea2;
+    end Tarea;
 
-    type t_tarea2 is access Tarea2;
+    type t_tarea is access Tarea;
 
-    N : t_tarea1;
-    T : t_tarea2;
+    N : t_tarea;
+    T : t_tarea;
 
 begin
-  N := new Tarea1(1);
-   T := new Tarea2(2);
+   N := new Tarea(1);
+   T := new Tarea(2);
    delay 4.0;
    Abort_Task (Current_Task);
 end main;
